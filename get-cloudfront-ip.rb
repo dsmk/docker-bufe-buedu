f3b705eaf7b55a491420b0c009b65887df418b5c
David King <dsmk@imp.bu.edu>
2017-08-03 15:37:25 -0400

    Trying to update any IP

--- get-cloudfront-ip.rb
+++ get-cloudfront-ip.rb
@@ -11,13 +11,14 @@ result = JSON.parse(buffer)
 result['prefixes'].each do |elem|
   #print "#{elem}\n"
   if elem['service'] == 'CLOUDFRONT'
-    print "set_real_ip_from #{elem['ip_prefix']} ;\n"
+    print "#### set_real_ip_from #{elem['ip_prefix']} ;\n"
   end
 end
 
 # At the end print our standard footer
 #
-print "\n# Set VPC subnet as trusted\nset_real_ip_from 10.0.0.0/8;\n"
+#print "\n# Set VPC subnet as trusted\nset_real_ip_from 10.0.0.0/8;\n"
+print "\n# Set VPC subnet as trusted\nset_real_ip_from 0.0.0.0/0;\n"
 print "\n# Set the header we are looking at\nreal_ip_header X-Forwarded-For ;\n"
 print "\n# Ignore all trusted IPs\nreal_ip_recursive on ;\n"
 
