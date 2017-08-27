e358db531b04527b38a865a61d1872787e3d97ae
David King <dsmk@imp.bu.edu>
2017-08-26 23:19:04 -0400

    Preparing cloudfront from inside the locked desktop

---
+++ apache/get-cloudfront-ip.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby
+#
+#
+require 'open-uri'
+require 'json'
+
+url = 'https://ip-ranges.amazonaws.com/ip-ranges.json'
+buffer = open(url).read
+result = JSON.parse(buffer)
+
+result['prefixes'].each do |elem|
+  #print "#{elem}\n"
+  if elem['service'] == 'CLOUDFRONT'
+    print "set_real_ip_from #{elem['ip_prefix']} ;\n"
+  end
+end
+
+# At the end print our standard footer
+#
+print "\n# Set VPC subnet as trusted\nset_real_ip_from 10.0.0.0/8;\n"
+#print "\n# Set VPC subnet as trusted\nset_real_ip_from 0.0.0.0/0;\n"
+print "\n# Set the header we are looking at\nreal_ip_header X-Forwarded-For ;\n"
+print "\n# Ignore all trusted IPs\nreal_ip_recursive on ;\n"
+
