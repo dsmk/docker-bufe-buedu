b5299315ff0a8ed27a2b1f252e15488570384d45
David King <dsmk@imp.bu.edu>
2017-08-27 07:16:22 -0400

    Modified get-cloudfront-ips to work with read-only root container

--- apache/get-cloudfront-ip.rb
+++ apache/get-cloudfront-ip.rb
@@ -1,24 +1,27 @@
 #!/usr/bin/ruby
 #
 #
-require 'open-uri'
+#require 'open-uri'
 require 'json'
 
-url = 'https://ip-ranges.amazonaws.com/ip-ranges.json'
-buffer = open(url).read
+#url = 'https://ip-ranges.amazonaws.com/ip-ranges.json'
+fname = "/tmp/ip-ranges.json"
+buffer = open(fname).read
 result = JSON.parse(buffer)
 
+print "# CloudFront IP addresses\n"
 result['prefixes'].each do |elem|
   #print "#{elem}\n"
   if elem['service'] == 'CLOUDFRONT'
-    print "set_real_ip_from #{elem['ip_prefix']} ;\n"
+    #print "set_real_ip_from #{elem['ip_prefix']} ;\n"
+    print "#{elem['ip_prefix']}\n"
   end
 end
 
 # At the end print our standard footer
 #
-print "\n# Set VPC subnet as trusted\nset_real_ip_from 10.0.0.0/8;\n"
+print "\n# Set VPC subnet as trusted\n10.0.0.0/8\n"
 #print "\n# Set VPC subnet as trusted\nset_real_ip_from 0.0.0.0/0;\n"
-print "\n# Set the header we are looking at\nreal_ip_header X-Forwarded-For ;\n"
-print "\n# Ignore all trusted IPs\nreal_ip_recursive on ;\n"
+#print "\n# Set the header we are looking at\nreal_ip_header X-Forwarded-For ;\n"
+#print "\n# Ignore all trusted IPs\nreal_ip_recursive on ;\n"
 
--- apache/run-httpd.sh
+++ apache/run-httpd.sh
@@ -2,6 +2,18 @@
 
 set -e
 
+# get the CloudFront IP addresses
+TMPCONFDIR=/tmp
+export TMPCONFDIR
+if curl -o "${TMPCONFDIR}/ip-ranges.json" "https://ip-ranges.amazonaws.com/ip-ranges.json" ; then
+  echo "bootstrap-cloudfront: got the URL for the latest ips"
+else
+  echo "bootstrap-cloudfront: did not get the URL, using backup ips"
+  cp /etc/httpd/ip-ranges.json "$TMPCONFDIR"
+fi
+/usr/sbin/get-cloudfront-ip.rb "${TMPCONFDIR}/ip-ranges.json" >"${TMPCONFDIR}/ip-ranges.conf"
+
+
 # Apache gets grumpy about PID files pre-existing
 #?
 
